cb7b7e45bbdc775f950f33a882cd3dc3cb52bdc9
João Victor da Costa Mazzei <j.v.c.mazzei@gmail.com>
2015-04-16 14:26:32 -0300

    Enums feitos pra classificar diversos dados e características necessários para a especificação de um carro.

---
+++ src/carro/Cambio.java
@@ -0,0 +1,7 @@
+package carro;
+
+public enum Cambio {
+
+	AUTOMÁTICO, SEMI_AUTOMÁTICO, MANUAL;
+	
+}
---
+++ src/carro/Modelo.java
@@ -0,0 +1,7 @@
+package carro;
+
+public enum Modelo {
+
+	CIVIC, S2000, GOL, GOLF, BEETLE, SILVIA, SKYLINE, GTR, F40, F50, ENZO;
+	
+}
