800ec896d33cd4daea7d9721ed404b2aa9006f9b
ricardolecocq <ricardolecocq@hotmail.com>
2015-04-16 15:18:14 -0300

    Início

--- src/classes/Loja.java
+++ src/classes/Loja.java
@@ -1,6 +1,24 @@
 package classes;
 
+import java.util.ArrayList;
+
+import motocicleta.Motocicleta;
+import carro.Carro;
+
 public class Loja
 {
 
+	String nome = "Doomhammer Motors";
+	String endereco = "Rua dos bobos n 0";
+	
+	ArrayList<Carro> estoqueCarros = new ArrayList<>();
+	ArrayList<Motocicleta> estoqueMotos = new ArrayList<>();
+	
+	
+	public void adicionarCarro()
+	{
+		
+	}
+	
+		
 }
