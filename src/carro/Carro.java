7d0b8f955e5d7329fa2c2a1d035d01fd9ceb79b6
João Victor da Costa Mazzei <j.v.c.mazzei@gmail.com>
2015-04-16 14:59:34 -0300

    Foi feito um construtor para as variáveis Enum.

--- src/carro/Carro.java
+++ src/carro/Carro.java
@@ -3,4 +3,28 @@ package carro;
 public class Carro 
 {
 	
+	String chassi;
+	float preco;
+	Montadora montadora;
+	Modelo modelo;
+	Motorizacao motorizacao;
+	Cambio cambio;
+	Tipo tipo;
+	Cor cor;
+	
+	public Carro (String _chassi, float _preco, Montadora _montadora, Modelo _modelo,
+	Motorizacao _motorizacao, Cambio _cambio, Tipo _tipo, Cor _cor)
+	{
+		
+		chassi = _chassi;
+		preco = _preco;
+		montadora = _montadora;
+		modelo = _modelo;
+		motorizacao = _motorizacao;
+		cambio = _cambio;
+		tipo = _tipo;
+		cor = _cor;
+		
+	}
+	
 }
