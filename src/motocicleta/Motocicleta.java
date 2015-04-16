26b3132854479b9f556fff3868fcae8cccfcf829
edufreis <eduardo.f.reis.rj@gmail.com>
2015-04-16 15:01:44 -0300

    Criação do construtor de Motocicleta

--- src/motocicleta/Motocicleta.java
+++ src/motocicleta/Motocicleta.java
@@ -3,6 +3,21 @@ package motocicleta;
 public class Motocicleta
 {
 	Cor cor;
+	Cilindrada cilindrada;
+	CapTanque capTanque;
+	Modelo modelo;
+	Montadora montadora;
+	Tipo tipo;
 	
+	public Motocicleta(Cor _cor,Cilindrada _cilindrada,CapTanque _capTanque,Modelo _modelo,Montadora _montadora,Tipo _tipo)
+	{
+		cor = _cor;
+		cilindrada = _cilindrada;
+		capTanque = _capTanque;
+		modelo = _modelo;
+		tipo = _tipo;
+		
+		
+	}
 
 }
